85f7c3b9009b5543dac3fea2e91e8834acec21b0
Dog49 <douglaszzc@outlook.com>
2024-08-26 00:47:58 -0400

    9-2 find SubCategories
    Try to use lambda+Stream to determine if there is any SubCategory.
    
    Create a test for this method.

--- src/main/java/com/imooc/mall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/imooc/mall/service/impl/CategoryServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.imooc.mall.consts.MallConst.ROOT_PARENT_ID;
 
@@ -20,18 +22,55 @@ public class CategoryServiceImpl implements ICategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    /*
+    *  Time: http > disk > memory > cpu
+    * mysql > redis > memcache > cpu cache
+    * Never do http request in for loop
+    * Never do mysql request in for loop
+    *
+    * */
     @Override
     public ResponseVo<List<CategoryVo>> selectAll() {
-        List<CategoryVo> categoryVoList = new ArrayList<>();
+//        List<CategoryVo> categoryVoList = new ArrayList<>();
         List<Category> categories = categoryMapper.selectAll();
 
-        for(Category category : categories){
-            if(category.getParentId().equals(ROOT_PARENT_ID)){
-                CategoryVo categoryVo = new CategoryVo();
-                BeanUtils.copyProperties(category,categoryVo);
-                categoryVoList.add(categoryVo);
-            }
-        }
+//        for(Category category : categories){
+//            if(category.getParentId().equals(ROOT_PARENT_ID)){
+//                CategoryVo categoryVo = new CategoryVo();
+//                BeanUtils.copyProperties(category,categoryVo);
+//                categoryVoList.add(categoryVo);
+//            }
+//        }
+
+        //lambda + Stream
+        List<CategoryVo> categoryVoList = categories.stream()
+                .filter(e -> e.getParentId().equals(ROOT_PARENT_ID)) //过滤出parentId为0的
+                .map(this::category2CategoryVo) //将category转为categoryVo
+                .sorted(Comparator.comparing(CategoryVo::getSortOrder).reversed())
+                .collect(Collectors.toList()); //Collect to the Collection
+
+        findSubCategory(categories, categoryVoList); //递归调用;
         return ResponseVo.success(categoryVoList);
     }
+
+    private CategoryVo category2CategoryVo(Category category){
+        CategoryVo categoryVo = new CategoryVo();
+        BeanUtils.copyProperties(category,categoryVo);
+        return categoryVo;
+    }
+
+    private void findSubCategory(List<Category> categories, List<CategoryVo> categoryVoList){
+        for(CategoryVo categoryVo : categoryVoList){
+            List<CategoryVo> subCategoryVoList = new ArrayList<>();
+            for(Category category : categories){
+                if(categoryVo.getId().equals(category.getParentId())){
+                    subCategoryVoList.add(category2CategoryVo(category)); //将category转为categoryVo, and add it to VoList
+                }
+                subCategoryVoList.sort(Comparator.comparing(CategoryVo::getSortOrder).reversed());
+                categoryVo.setSubCategories(subCategoryVoList);
+                findSubCategory(categories, subCategoryVoList);
+            }
+        }
+    }
+
 }
