d9522c39cf73cd0442f61ab4c150a1339680c6b7
Dog49 <douglaszzc@outlook.com>
2024-08-26 18:15:24 -0400

    9-1 Return the list of product
    Use set to record the product of all the numbers in the list, and then convert it to a set.

--- src/main/java/com/imooc/mall/service/ICategoryService.java
+++ src/main/java/com/imooc/mall/service/ICategoryService.java
@@ -2,13 +2,16 @@ package com.imooc.mall.service;
 
 import com.imooc.mall.vo.CategoryVo;
 import com.imooc.mall.vo.ResponseVo;
-import org.springframework.context.annotation.Bean;
+
 
 import java.util.List;
+import java.util.Set;
 
 public interface ICategoryService {
 
     ResponseVo<List<CategoryVo>> selectAll();
+
+    void findSubCategoryId(Integer id, Set<Integer> resultSet);
 
 
 }
